f129a3a62d5f51fe69ba82697e47a98c551b2cb4
Breno Flaubert <brenoflaubert@gmail.com>
2023-11-21 13:29:16 -0300

    feat: alteração interface receita

--- src/types/IReceita.ts
+++ src/types/IReceita.ts
@@ -1,12 +1,10 @@
-import { IOwner } from "./IOwner";
-
-export interface IReceita {
+export interface InterfaceReceita {
   id: number;
-  name: string;
-  description: string;
-  methodPreparation?: string;
-  ingredients?: string;
+  nome: string;
+  descricao: string;
+  modoDePreparo: string;
+  ingredientes: string;
   imgUrl?: string;
-  assessment?: number;
-  owner?: IOwner;
+  autor: string;
+  criadoEm: string;
 }
